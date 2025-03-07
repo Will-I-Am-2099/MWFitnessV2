e9d494b295a5e363140bed971ce46b9cd3a7ec6b
sgwaldrop <shelbygwaldrop@gmail.com>
2025-03-07 13:02:40 -0600

    Update Fitness.py

--- Fitness.py
+++ Fitness.py
@@ -199,11 +199,10 @@ if search_name:
             
             # If proof exists, display the clickable proof link
             if proof_filename != "No Proof":
-                proof_link = f'<a href="/uploads/{proof_filename}" target="_blank">View proof</a>'
+                proof_link = f'<a href="/{UPLOAD_DIR}/{proof_filename}" target="_blank">View proof</a>'
                 st.markdown(proof_link, unsafe_allow_html=True)
                 
                 # Optionally, display the image inline as well
-                st.image(f"uploads/{proof_filename}", caption=f"Proof for {row['Name']} at {timestamp}", use_column_width=True)
+                st.image(f"{UPLOAD_DIR}/{proof_filename}", caption=f"Proof for {row['Name']} at {timestamp}", use_column_width=True)
     else:
         st.warning("No records found for this user. Please try again with a different name.")
-           
